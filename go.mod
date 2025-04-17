f06974d50033c44f6e95f842060e3abe53fb0ccc
Stefan Taber <nz.themess@gmail.com>
2025-04-17 21:51:56 +1200

    USB input displaying messages

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/The-Mess-NZ/midipunk
 
 go 1.24.2
 
-require gitlab.com/gomidi/midi/v2 v2.2.19 // indirect
+require gitlab.com/gomidi/midi/v2 v2.2.19
+
+require (
+	github.com/creack/goselect v0.1.2 // indirect
+	go.bug.st/serial v1.6.4 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+)
--- midipunk.go
+++ midipunk.go
@@ -2,12 +2,69 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	midi "gitlab.com/gomidi/midi/v2"
 	_ "gitlab.com/gomidi/midi/v2/drivers/rtmididrv"
+	"go.bug.st/serial"
 )
+
+// func readSerial(ch chan midi.Message) {
+func readSerial() {
+	mode := &serial.Mode{
+		BaudRate: 38400,
+	}
+
+	port, err := serial.Open("/dev/ttyAMA5", mode)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer port.Close()
+
+	buf := make([]byte, 8)
+
+	for {
+		n, err := port.Read(buf)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if n > 0 {
+			fmt.Printf("Received: %x\n", buf[:n])
+			// msg := midi.Message(buf[:n])
+			// ch <- msg
+		}
+	}
+}
+
+func handleMessage(msg midi.Message, timestamps int32) {
+	// Handle the MIDI message
+	fmt.Printf("MIDI Message: %s\n", msg)
+}
 
 func main() {
 	defer midi.CloseDriver()
+
 	fmt.Println(midi.GetInPorts())
+
+	var in, err = midi.InPort(2)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stop, _ := midi.ListenTo(in, handleMessage)
+	defer stop()
+
+	// Create a communication channel
+	// ch := make(chan midi.Message)
+
+	// Start reading from serial port
+	go readSerial()
+
+	// Keep main thread running
+	select {}
 }
