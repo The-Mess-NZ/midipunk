c9b442aa50d52b40fd068f6521d96997673aedff
Stefan Taber <nz.themess@gmail.com>
2025-07-11 17:06:43 +1200

    Basic running test setup for connected USB midi devices.

--- midipunk.go
+++ midipunk.go
@@ -4,83 +4,43 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/The-Mess-NZ/midipunk/midiport"
+	"github.com/The-Mess-NZ/midipunk/midirouter"
+
 	midi "gitlab.com/gomidi/midi/v2"
 	_ "gitlab.com/gomidi/midi/v2/drivers/rtmididrv"
-	"go.bug.st/serial"
 )
-
-func readSerial(ch chan midi.Message) {
-	mode := &serial.Mode{
-		BaudRate: 38400,
-	}
-
-	port, err := serial.Open("/dev/ttyAMA2", mode)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer port.Close()
-
-	buf := make([]byte, 3)
-
-	for {
-		n, err := port.Read(buf)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if n > 0 {
-			// fmt.Printf("Received: %x\n", buf[:n])
-			msg := midi.Message(buf[:n])
-			fmt.Printf("Received Serial Message: %s\n", msg)
-			ch <- msg
-		}
-	}
-}
-
-func handleUsbMessage(msg midi.Message, timestamps int32, ch chan midi.Message) {
-	// Handle the MIDI message
-	fmt.Printf("MIDI Message: %s\n", msg)
-
-	ch <- msg
-}
-
-func handleChannelMessage(ch chan midi.Message) {
-	for msg := range ch {
-		// Process the message received from the channel
-		fmt.Printf("Channel Message: %s\n", msg)
-	}
-}
 
 func main() {
 	defer midi.CloseDriver()
 
-	fmt.Println(midi.GetInPorts())
+	fmt.Println("Available MIDI ports:", midi.GetInPorts())
 
-	var in, err = midi.InPort(2)
+	// Create port configurations
+	usbInputPort := midiport.NewUSBPortConfig("1", midiport.INPUT)
+	dinInputPort := midiport.NewDINPortConfig("din1", "/dev/ttyAMA2", midiport.INPUT)
 
+	fmt.Printf("Created USB port: %s\n", usbInputPort.String())
+	fmt.Printf("Created DIN port: %s\n", dinInputPort.String())
+
+	// Create route inputs
+	usbInput := midirouter.NewRouteInput(1, usbInputPort)
+	dinInput := midirouter.NewRouteInput(1, dinInputPort)
+
+	// Create a simple route that takes both inputs (no outputs for now)
+	route := midirouter.NewRoute([]*midirouter.RouteInput{usbInput, dinInput}, []*midirouter.RouteOutput{})
+
+	// Create and start the router
+	router := midirouter.NewRouter([]*midirouter.Route{route})
+
+	err := router.Start()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to start router: %v", err)
 	}
 
-	// Create a communication channel
-	ch := make(chan midi.Message)
+	defer router.Stop()
 
-	stop, _ := midi.ListenTo(
-		in,
-		func(msg midi.Message, timestamps int32) {
-			handleUsbMessage(msg, timestamps, ch)
-		},
-	)
-	defer stop()
-
-	// Start reading from serial port
-	go readSerial(ch)
-
-	// Start a goroutine to handle messages from the channel
-	go handleChannelMessage(ch)
+	fmt.Println("MidiPunk router started. Press Ctrl+C to exit...")
 
 	// Keep main thread running
 	select {}
