1e259a303ead121bb65431c50b3d71499f48f8da
Stefan Taber <nz.themess@gmail.com>
2025-07-12 13:51:10 +1200

    Remove unnecessary code

--- api/event_api.go
+++
@@ -1,4 +0,0 @@
-package api
-
-// This file will contain the implementation for broadcasting events via WebSocket.
-// TODO: Implement events WebSocket handler for /events/ws
--- api/status_api.go
+++
@@ -1,4 +0,0 @@
-package api
-
-// This file will contain the implementation for broadcasting realtime status via WebSocket.
-// TODO: Implement status WebSocket handler for /status/ws
